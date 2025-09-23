3f90974f1b58e4a8ac9500d431fa68ad7fccf66c
Himadri <122869644+Himadri2@users.noreply.github.com>
2025-09-23 15:03:13 +0530

    Update ansible_deploy.groovy

--- vars/ansible_deploy.groovy
+++ vars/ansible_deploy.groovy
@@ -3,6 +3,6 @@ def call() {
     writeFile file: 'deploy.yml', text: libraryResource('ansible/deploy.yml')
     writeFile file: 'aws_ec2.yml', text: libraryResource('ansible/aws_ec2.yml')
 
-    // Run Ansible playbook
-    sh "ansible-playbook -i aws_ec2.yml deploy.yml"
+    // Run Ansible playbook as ubuntu user with the private key
+    sh "ansible-playbook -i aws_ec2.yml deploy.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa -e 'ansible_ssh_common_args=\"-o StrictHostKeyChecking=no\"'"
 }
